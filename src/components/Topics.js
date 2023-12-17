746b51ffba5c92c5bb17f888bbeb3b9a02a2ad7e
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2023-12-17 16:04:23 +0530

    Topics Component made responsive and CSS rectified

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,25 +1,67 @@
 import React from "react";
 
 
+// const Topics = (props) =>{
+
+//     return (
+//         <>
+//         <div className="topics-container">
+//           <p>This is topic {props.item}</p>
+//           <div className="topic-heading"><h1>{props.heading}</h1>   </div>
+//           <div className="topic-body">{props.body}</div>
+//           <div className="topic-example" >
+//             <pre> 
+//               <code>{props.example}</code>          
+//             </pre>       
+//           </div>
+        
+//         </div>
+//         </>
+//     )
+// }
+
+
+
 const Topics = (props) =>{
-    return (
-        <>
-        <div className="topics-container">
-        <p>This is topic {props.item}</p>
+  const bodyArray = Array.isArray(props.body) ? props.body : [props.body];
+  return (
+      <>
+      <div className="topics-container">
+        <p>This is topic {props.topicId}</p>
         <div className="topic-heading"><h1>{props.heading}</h1>   </div>
-        <div className="topic-body">{props.body} </div>
-        <div className="topic-example" >
-        {props.children ? (
-          props.children
-        ) : (
-          <div dangerouslySetInnerHTML={{ __html: props.example }} ></div>
-        )}    
-         </div>
-        
+        <div className="topic-body">
+          <ul className="topics-ul">
+          {bodyArray.map((detail, index) => (
+                <li key={index}>{detail}</li>
+              ))}
+          </ul>
         </div>
-        
-        </>
-    )
+        <div className="topic-example" >
+          <pre> 
+            <code>{props.example}</code>          
+          </pre>       
+        </div>
+      
+      </div>
+      </>
+  )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Topics;
