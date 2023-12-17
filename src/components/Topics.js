7962d7e7b0f2afc0d030232a127854b754f6cd62
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2023-12-17 23:20:29 +0530

    Topics component cleaned

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,27 +1,4 @@
 import React from "react";
-
-
-// const Topics = (props) =>{
-
-//     return (
-//         <>
-//         <div className="topics-container">
-//           <p>This is topic {props.item}</p>
-//           <div className="topic-heading"><h1>{props.heading}</h1>   </div>
-//           <div className="topic-body">{props.body}</div>
-//           <div className="topic-example" >
-//             <pre> 
-//               <code>{props.example}</code>          
-//             </pre>       
-//           </div>
-        
-//         </div>
-//         </>
-//     )
-// }
-
-
-
 const Topics = (props) =>{
   const bodyArray = Array.isArray(props.body) ? props.body : [props.body];
   return (
