6be9c675e188cc0c459b5e6e6b0676a4e70049c5
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2023-12-22 23:40:55 +0530

    Working on login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,9 @@ import React from "react";
 const Login = () => {
    return(
         <div className="login-Container">
-            <p>I am login components</p>
-            <input type="text" placeholder="Enter Username"/>
-            <input type="password" placeholder="Enter Password" />
+            <span className="login-span">Sign Up</span>
+            <input className="login-input" type="text" placeholder="Enter Username"/>
+            <input className="login-input" type="password" placeholder="Enter Password" />
         </div>
    ) 
 }
