282e60c338e418b92cd8d63ffcdf21dc61516f90
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2023-12-28 22:48:14 +0530

    React contents started to fill

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import './App.css';
-import AboutUs from './components/AboutUs';
-import Course from './components/Course';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer'
-import {
-  Route,
-  Routes
-} from "react-router-dom";
-import SubjectTopics from './components/SubjectTopics';
-import Login from './components/Login';
-
-
-
+import "./App.css";
+import AboutUs from "./components/AboutUs";
+import Course from "./components/Course";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import SubjectTopics from "./components/SubjectTopics";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <Routes>
-        <Route path="/" element={<Course/>} ></Route>
-        <Route path="/aboutus" element={<AboutUs></AboutUs>} > </Route>
-        <Route path="/oops" element={<SubjectTopics subject='oops' />} > </Route>
-        <Route path="/javascript" element={<SubjectTopics subject='javascript' />} > </Route>
-        <Route path="/os" element={<SubjectTopics subject='os' />} > </Route>
-        <Route path="/linux" element={<SubjectTopics subject='linux' />} > </Route>
-        <Route path='/login' element={<Login/>} ></Route>
+        <Route path="/" element={<Course />}></Route>
+        <Route path="/aboutus" element={<AboutUs></AboutUs>}></Route>
+        <Route path="/oops" element={<SubjectTopics subject="oops" />}></Route>
+        <Route
+          path="/javascript"
+          element={<SubjectTopics subject="javascript" />}
+        ></Route>
+        <Route path="/os" element={<SubjectTopics subject="os" />}></Route>
+        <Route
+          path="/linux"
+          element={<SubjectTopics subject="linux" />}
+        ></Route>
+        <Route
+          path="/react"
+          element={<SubjectTopics subject="react" />}
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,44 +1,32 @@
 import React from "react";
-const Topics = (props) =>{
+const Topics = (props) => {
   const bodyArray = Array.isArray(props.body) ? props.body : [props.body];
   return (
-      <>
+    <>
       <div className="topics-container">
         {/* <p>This is topic {props.topicId}</p> */}
-        <div className="topic-heading"><h1>{props.heading}</h1>   </div>
+        <div className="topic-heading">
+          <h1>{props.heading}</h1>{" "}
+        </div>
         <div className="topic-body">
           <ul className="topics-ul">
-          {bodyArray.map((detail, index) => (
-                <li className="topics-listItem" key={index}>{detail}</li>
-              ))}
+            {bodyArray.map((detail, index) => (
+              <li className="topics-listItem" key={index}>
+                {detail}
+              </li>
+            ))}
           </ul>
         </div>
-        { props.example &&  <div className="topic-example" >
-          <pre className="topic-pre"> 
-            <code className="topic-code">{props.example}</code>          
-          </pre>       
-        </div>}
-      
+        {props.example && (
+          <div className="topic-example">
+            <pre className="topic-pre">
+              <code className="topic-code">{props.example}</code>
+            </pre>
+          </div>
+        )}
       </div>
-      </>
-  )
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    </>
+  );
+};
 
 export default Topics;
