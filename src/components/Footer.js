fdcb125ea2eb6cad49bb56176944c6fbff52a9f4
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2023-12-31 23:16:44 +0530

    Icons added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,36 @@
 import React from "react";
 import AnimatedIcon from "./AnimatedIcon";
 import { Link } from "react-router-dom";
-const Footer = () =>{
-    return(
-        <div className="container">
-            <footer>
-            <div className="footer-content contact">
-                <h3>Contact Us.</h3>
-                <p>Email: quickrev@gmail.com</p>
-                <p>Phone: +91 94565 86457</p>
-            </div>
-            <div className="footer-content social">
-                <h3>Follow Us on:</h3>
-                <p>Instagram</p>
-                <p>github</p>
-            </div>
-            <div className="footer-content quick-links">
-                <h3>Quick Links</h3>
-                <Link to='/'> Home</Link>
-                <Link to='/aboutus' >About Us </Link>
-            </div>
-            <div className="footer-content social">
-                {<AnimatedIcon/>}
-            </div>
-            
-        </footer>
+import { FaInstagram } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io5";
+
+const Footer = () => {
+  return (
+    <div className="container">
+      <footer>
+        <div className="footer-content contact">
+          <h3>Contact Us.</h3>
+          <p>Email: quickrev@gmail.com</p>
+          <p>Phone: +91 94565 86457</p>
         </div>
-    )
-}
+        <div className="footer-content social">
+          <h3>Follow Us on:</h3>
+          <p>
+            <FaInstagram /> Instagram
+          </p>
+          <p>
+            <IoLogoGithub /> github
+          </p>
+        </div>
+        <div className="footer-content quick-links">
+          <h3>Quick Links</h3>
+          <Link to="/"> Home</Link>
+          <Link to="/aboutus">About Us </Link>
+        </div>
+        <div className="footer-content social">{<AnimatedIcon />}</div>
+      </footer>
+    </div>
+  );
+};
 
 export default Footer;
