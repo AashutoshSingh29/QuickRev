7e0e1f2b9b7f010c09a38d92d0d43fccab3f3a9c
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2024-01-10 23:28:50 +0530

    Contact Us

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,27 +2,9 @@ import React from "react";
 
 const ContactUs = () => {
   return (
-    <div className="contactus-container">
-      <div className="contact-info">
-        <h1>Contact Us </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa a optio,
-          adipisci, nobis labore id iusto quas porro quibusdam quam esse
-          temporibus cumque voluptatum animi nisi quasi voluptas, quo fugit!
-        </p>
-      </div>
-      <div className="contact-form">
-        <div className="form">
-          <label> Your Name</label>
-          <input type="text" placeholder="Your Name" />
-          <label> Phone Number</label>
-          <input type="number" placeholder="Your Number" />
-          <label>Email</label>
-          <input type="text" placeholder="example@gmail.com" />
-          <label>Message</label>
-          <input type="textarea" placeholder="Type your message here....." />
-        </div>
-      </div>
+    <div className="contact-us">
+      <div className="block">hello</div>
+      <div className="form">World</div>
     </div>
   );
 };
