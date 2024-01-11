86a65e33ce01a70230bb171b3ce3a12efe169e4f
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2024-01-11 23:45:37 +0530

    Contact Us page improved

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { MdCall } from "react-icons/md";
+import { CiMail } from "react-icons/ci";
+import { FaLocationDot } from "react-icons/fa6";
 
 const ContactUs = () => {
   return (
     <div className="contact-us">
       <div className="block">
-        <div className="box-1">
-          <h2>Contact</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-            consequuntur ratione minus necessitatibus at commodi tenetur magni
-            laboriosam dolorum libero architecto recusandae iste molestias unde
-            esse, corrupti excepturi veritatis itaque!
-          </p>
+        <div className="box">
+          <MdCall size={32} color="rgb(49, 115, 239)" />
+          <p>+91 94565 86457</p>
+        </div>
+        <div className="box">
+          <CiMail size={32} color="rgb(49, 115, 239)" />
+          <p>quickrev@gmail.com</p>
+        </div>
+        <div className="box">
+          <FaLocationDot size={32} color="rgb(49, 115, 239)" />
+          <p>Kattigenahalli, Bengaluru-560064</p>
         </div>
       </div>
       <div className="form">World</div>
