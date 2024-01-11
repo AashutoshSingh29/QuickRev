8f5c184b8ab09709c9b51f57458179d2f1577c41
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2024-01-11 23:55:03 +0530

    Navbar QuickRev made LINK

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,46 @@
-import {React,useState} from 'react'
-import { Link } from 'react-router-dom';
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
 
-const NavBar = () =>{
-
+const NavBar = () => {
   const [isNavVisible, setNavVisibility] = useState(false);
 
   const toggleNavbar = () => {
     setNavVisibility(!isNavVisible);
   };
   return (
-    <div className='container-fluid'>
-      <nav className='navbar'>     
-      <div className="menu">
-        
-        <label className="logo">
-              <a href="/">
-                <img
-                  src="/nameIcon6.png"
-                  alt="Company Logo"
-                  // width="50"
-                  // height="50"
-                  
-                />
-              </a>
-            </label>
-            <ul className={`listItems ${isNavVisible ? 'visible' : ''}`}>
-            <li className='companyName'><a href="/">QuickRev</a></li>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/aboutus">About Us</Link></li>
-            <li><Link to="/contactus">Contact Us</Link></li>
-
-          </ul><ul>
-
-            <div className="bars-icon" onClick={toggleNavbar} >
+    <div className="container-fluid">
+      <nav className="navbar">
+        <div className="menu">
+          <label className="logo">
+            <Link to="/">
+              <img src="/nameIcon6.png" alt="Company Logo" />
+            </Link>
+          </label>
+          <ul className={`listItems ${isNavVisible ? "visible" : ""}`}>
+            <li className="companyName">
+              <Link to="/">QuickRev</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/aboutus">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contactus">Contact Us</Link>
+            </li>
+          </ul>
+          <ul>
+            <div className="bars-icon" onClick={toggleNavbar}>
               <div className="bar"></div>
               <div className="bar"></div>
               <div className="bar"></div>
-            </div> 
-        </ul>    
-        
-      </div>
+            </div>
+          </ul>
+        </div>
       </nav>
-       
     </div>
-  )
+  );
 
   // function toggleNavbar() {
   //   var navbar = document.querySelectorAll('.listItems');
@@ -51,6 +48,6 @@ const NavBar = () =>{
   //   item.classList.toggle('visible');
   // });
   // }
-}
+};
 
 export default NavBar;
