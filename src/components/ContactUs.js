7edb41f0a4ff060760520422a1ee39bd8c260934
Aashutosh Singh Gautam <singhaashutosh29@gmail.com>
2024-01-12 22:49:31 +0530

    Contact us form created

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -8,19 +8,38 @@ const ContactUs = () => {
     <div className="contact-us">
       <div className="block">
         <div className="box">
-          <MdCall size={32} color="rgb(49, 115, 239)" />
+          <div className="icon-back">
+            <MdCall size={30} color="rgb(49, 115, 239)" />
+          </div>
           <p>+91 94565 86457</p>
         </div>
         <div className="box">
-          <CiMail size={32} color="rgb(49, 115, 239)" />
+          <div className="icon-back">
+            <CiMail size={30} color="rgb(49, 115, 239)" />
+          </div>
           <p>quickrev@gmail.com</p>
         </div>
         <div className="box">
-          <FaLocationDot size={32} color="rgb(49, 115, 239)" />
+          <div className="icon-back">
+            <FaLocationDot size={30} color="rgb(49, 115, 239)" />
+          </div>
           <p>Kattigenahalli, Bengaluru-560064</p>
         </div>
       </div>
-      <div className="form">Hello World</div>
+      <div className="form-section">
+        <div className="form">
+          <h2>Write to us.....</h2>
+          <span>Your Name</span>
+          <input type="text" placeholder="Full Name" />
+          <span>Email</span>
+          <input type="text" placeholder="example@example.com" />
+          <span>Number</span>
+          <input type="contact" placeholder="Contact Number" />
+          <span>Description</span>
+          <input type="text" placeholder="Type your message here....." />
+          <button>Send Message</button>
+        </div>
+      </div>
     </div>
   );
 };
